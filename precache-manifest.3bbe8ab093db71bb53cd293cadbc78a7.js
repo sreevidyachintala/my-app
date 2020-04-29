0cb9f062eaf46e5a15e1439f765acebcd7689f23
sreevidyachintala <sreevidyachintala16@gmail.com>
2020-04-29 11:15:40 +0530

    Updates

---
+++ precache-manifest.3bbe8ab093db71bb53cd293cadbc78a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc896d7578137b4f4cc79a3c67ba4cf3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ced34547695e6cf3eced",
+    "url": "/my-app/static/css/main.5a09a811.chunk.css"
+  },
+  {
+    "revision": "a0a897c512337c908121",
+    "url": "/my-app/static/js/2.ee84c7bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.ee84c7bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced34547695e6cf3eced",
+    "url": "/my-app/static/js/main.e579adea.chunk.js"
+  },
+  {
+    "revision": "6b90711d51ae396ac389",
+    "url": "/my-app/static/js/runtime-main.8c451e69.js"
+  }
+]);
